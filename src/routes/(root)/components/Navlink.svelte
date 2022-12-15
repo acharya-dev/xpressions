aeaccaf904798a97f03e28dcd44e668aa94e494d
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-12-15 23:16:04 +0530

    create: root directory with home page

---
+++ src/routes/(root)/components/Navlink.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let href: string | undefined = undefined;
+	export let outerStyle: string | undefined = undefined;
+	export let innerStyle: string | undefined = undefined;
+</script>
+
+<li class="app-navlink cursor-pointer {outerStyle}">
+	<a {href} class={innerStyle}><slot /></a>
+</li>
