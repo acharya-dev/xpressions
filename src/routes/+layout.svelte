2675bef4d2cd19cb88c60c1f908ce590af1ecd8e
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-12-16 11:58:43 +0530

    update: animations and reactivity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,16 +6,52 @@
 		outerStyle: 'px-4 hover:text-neutral-600 transition-all',
 		innerStyle: ''
 	};
+
+	let innerWidth: number;
+	let isSmall: boolean = false;
+	let navElement: HTMLUListElement;
+
+	const hideElement = <T extends HTMLElement>(element: T, timeout: number) =>
+		setTimeout(() => (element.style.display = 'none'), timeout);
+	const showElement = <T extends HTMLElement>(element: T, display: string) =>
+		(element.style.display = display);
+
+	const handleResize = (e: UIEvent & { currentTarget: Window }) => {
+		if (e.currentTarget.innerWidth < 640) {
+			hideElement<HTMLUListElement>(navElement, 210);
+		} else if (e.currentTarget.innerWidth >= 640) {
+			showElement(navElement, 'flex');
+		}
+	};
 </script>
 
+<svelte:window on:resize={handleResize} on:resize={handleResize} />
+
 <main class="app min-h-screen">
-	<header class="h-14 w-screen flex shadow-md items-center justify-between">
-		<a href="/" class="app-brand px-8 font-extrabold text-4xl">xpressions</a>
-		<ul class="app-nav hidden sm:flex sm:items-center pr-4">
+	<header class="h-14 w-screen flex shadow-md items-center relative z-10 ">
+		<a
+			href="/"
+			class="app-brand px-8 font-extrabold text-4xl absolute sm:left-0 animate-brandCentre sm:animate-brandBack"
+			>xpressions</a
+		>
+		<ul
+			bind:this={navElement}
+			class="app-nav flex animate-navExit sm:animate-navEnter sm:items-center pr-4 ml-[255px] "
+		>
 			<Navlink href="/" outerStyle={navStyles.outerStyle} innerStyle={navStyles.innerStyle}>
 				Home
 			</Navlink>
-            <Navlink href="/about" outerStyle={navStyles.outerStyle} innerStyle={navStyles.innerStyle}>
+			<Navlink href="/about" outerStyle={navStyles.outerStyle} innerStyle={navStyles.innerStyle}>
+				About
+			</Navlink>
+		</ul>
+		<ul
+			class="w-screen h-7 absolute bg-slate-300 bottom-[calc(0rem-1.75rem)] flex items-center justify-center shadow-md sm:hidden"
+		>
+			<Navlink href="/" outerStyle={navStyles.outerStyle} innerStyle={navStyles.innerStyle}>
+				Home
+			</Navlink>
+			<Navlink href="/about" outerStyle={navStyles.outerStyle} innerStyle={navStyles.innerStyle}>
 				About
 			</Navlink>
 		</ul>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			keyframes: {
+				navEnter: {
+					'0%': { opacity: 0, transform: 'translateX(300px)', visibility: 'hidden' },
+					'100%': { opacity: 1, transform: 'translateX(0px)', visibility: 'visible' }
+				},
+				navExit: {
+					from: { opacity: 1, transform: 'translateX(0px)', visibility: 'visible' },
+					to: { opacity: 0, transform: 'translateX(300px)', visibility: 'hidden' }
+				},
+				brandCentre: {
+					from: { transform: 'translateX(0)', left: 0 },
+					to: { transform: 'translateX(-50%)', left: '50%' }
+				},
+				brandBack: {
+					from: { transform: 'translateX(-50%)', left: '50%' },
+					to: { transform: 'translateX(0)', left: 0 }
+				},
+				
+			},
+			animation: {
+				navEnter: 'navEnter 200ms cubic-bezier(.01,-0.01,.61,.99) 1 forwards',
+				navExit: 'navExit 200ms cubic-bezier(.01,-0.01,.61,.99) forwards',
+				brandCentre: 'brandCentre 200ms cubic-bezier(.01,-0.01,.61,.99) forwards',
+				brandBack: 'brandBack 200ms cubic-bezier(.01,-0.01,.61,.99) forwards'
+			}
+		}
+	},
+	plugins: []
+};
